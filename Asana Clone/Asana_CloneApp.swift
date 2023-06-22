cce12e2cea67fc2a84abaf1d1cf14d648b47cc92
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-06-21 22:14:00 -0500

    Initial Commit

---
+++ Asana Clone/Asana_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asana_CloneApp.swift
+//  Asana Clone
+//
+//  Created by Nick on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Asana_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
